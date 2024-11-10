a83237e8006d84c257e4759b6dc6ec010b31d33b
Alberu <treblabr@gmail.com>
2024-11-10 17:55:53 +0000

    got most of it working
    Still need to fix an issue with selecting the right pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarProvider,
+  SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { Button } from "./components/ui/button";
 
@@ -26,10 +27,23 @@ function App() {
   const [pokemonInfo, setPokemonInfo] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const {name, id, weight} = pokemonInfo || {} // just in case there is no data
+  const [searchString, setSearchString] = useState('')
+
+  const {name, height, abilities, stats, types, moves, sprites} = pokemonInfo || {} // just in case there is no data
+  // THIS NEEDS TO BE CLEANED UP
+  const usableSprites = Object.keys(sprites || {}).filter(val => {
+    if (!sprites[val]) {return false}
+    if (['versions','other'].includes(val)) {return false}
+    return true
+  })
+
+  const filteredPokemonList = first151Pokemon.filter((val, valIndex) => {
+    if (getFullPokedexNumber(valIndex).includes(searchString)) {return true}
+    if (val.toLowerCase().includes(searchString.toLowerCase())) {return true}
+    return false
+  })
 
   useEffect(() => {
-    console.log(pokemonInfo);
     // check if already loading and that there is a localStorage loaded too
     if (loading || !localStorage) {
       return;
@@ -42,14 +56,15 @@ function App() {
       cache = JSON.parse(localStorage.getItem("pokedex"));
     }
 
-    // check if selected pokemon already in cache
-    if (selectedPokemon in cache) {
+    // check if selected pokemon already in cache and that the pokemon ins't empty
+    if (selectedPokemon in cache && cache[selectedPokemon]) {
+      console.log(`Loaded pokemon (${selectedPokemon}) from cache`)
       setPokemonInfo(cache[selectedPokemon]);
+      return // make sure to exit
     }
 
     // fetch selected pokemon data from the API
     async function fetchPokemonData() {
-      console.log("Trying to fetch data from API");
       try {
         // get info
         setLoading(true);
@@ -65,35 +80,44 @@ function App() {
       } catch (err) {
         console.log(err);
       } finally {
-        console.log("Succesfully fetched");
+        console.log("Succesfully fetched pokemon");
         setLoading(false);
       }
     }
 
-    fetchPokemonData;
+    fetchPokemonData();
   }, [selectedPokemon]);
+
+  if (loading || !pokemonInfo) {
+    return (
+      <div>
+        <h4>Loading...</h4>
+      </div>
+    )
+  }
 
   return (
     <>
       <SidebarProvider>
         <Sidebar>
           <SidebarHeader>
-            <Input placeholder="Pokemon ### or name" />
+            <Input value={searchString} onChange={(e) => {
+              setSearchString(e.target.value)
+            }} placeholder="Pokemon ### or name" />
           </SidebarHeader>
           <SidebarContent>
             <SidebarGroup>
               <SidebarGroupLabel>Pokedex</SidebarGroupLabel>
               <SidebarGroupContent>
                 <SidebarMenu>
-                  {first151Pokemon.map((pokemon, pokemonIndex) => {
+                  {filteredPokemonList.map((pokemon, pokemonIndex) => {
+                    const truePokemonIndex = first151Pokemon.indexOf(pokemon)
                     return (
                       <SidebarMenuItem key={pokemonIndex}>
-                        <SidebarMenuButton asChild>
-                          <a href="#">
+                        <SidebarMenuButton isActive={pokemonIndex===selectedPokemon} onClick={() => {setSelectedPokemon(truePokemonIndex)}}>
                             <span>
-                              {getFullPokedexNumber(pokemonIndex)} {pokemon}
+                              {getFullPokedexNumber(truePokemonIndex)} {pokemon}
                             </span>
-                          </a>
                         </SidebarMenuButton>
                       </SidebarMenuItem>
                     );
@@ -104,12 +128,52 @@ function App() {
           </SidebarContent>
         </Sidebar>
         <SidebarInset>
+
+        {/* to be able to switch between the view on and off */}
+        <SidebarTrigger />
+          {/* {if (loading){
+            return (<p>hi</p>)
+          }} // why can't I put this here */}
           <div>
             <h4>
               #{getFullPokedexNumber(selectedPokemon)} {name}
             </h4>
-            <p>{id}</p>
-            <p>{weight}</p>
+          </div>
+          <div>
+            {types.map((type, typeIndex) => {
+              return (
+                <div key={typeIndex}>
+                  <p>some card with {type?.type?.name}</p>
+                </div>
+              )
+            })}
+          </div>
+          <div>
+            {usableSprites.map((spriteKey, spriteIndex) => {
+              return (
+                <img key={spriteIndex} src={sprites[spriteKey]} alt={`${name}-img-${spriteKey}`} />
+              )
+            })}
+          </div>
+          <h3>Stats</h3>
+          <div>
+            {stats.map((statObj, statIndex) => {
+              const {stat, base_stat} = statObj
+              return (
+                <div key={statIndex}>
+                  <p>{stat?.name.replaceAll('-',' ')}: {base_stat}</p>
+                  <p>add a loading bar of how good this is</p>
+                </div>
+              )
+            })}
+          </div>
+          <h3>Moves</h3>
+          <div>
+            {moves.map((move, moveIndex) => {
+              return (
+                <Button key={moveIndex}>{move?.move?.name.replaceAll('-', ' ')}</Button>
+              )
+            })}
           </div>
         </SidebarInset>
       </SidebarProvider>
